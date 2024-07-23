4c8c92518acc9c0a01d9fabadda59f8f526c6e81
Mohamed Tarek <43273151+mtarek2005@users.noreply.github.com>
2024-07-23 11:01:25 +0300

    email stuff

--- project/settings.py
+++ project/settings.py
@@ -69,7 +69,8 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = TrueEMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -12,12 +12,14 @@ router.register('reservations', views.viewsets_reservation)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name="authentication/login.html"), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name="authentication/logout.html"), name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name="authentication/password_reset.html"), name='password_reset'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="users_hub/password_reset_done.html"), name='password_reset_done'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name="authentication/password_reset_done.html"), name='password_reset_done'),
     path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='users_hub/password_reset_confirm.html'), name='password_reset_confirm'),
+        template_name='authentication/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='users_hub/password_reset_complete.html'), name='password_reset_complete'),
+        template_name='authentication/password_reset_complete.html'), name='password_reset_complete'),
     
     path('django/jsonresponsenomodel/', views.no_rest_no_model),
     
